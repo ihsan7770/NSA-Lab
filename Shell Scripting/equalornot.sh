f8c42f8abf7a2fc938324a13dfee5b37b589ea65
mohammed ihsan <159607635+ihsan7770@users.noreply.github.com>
2024-04-09 14:03:40 +0530

    Add files via upload

---
+++ Shell Scripting/equalornot.sh
@@ -0,0 +1,12 @@
+read -p "enter the first number: " a
+read -p "enter the second number: " b
+if [ $a == $b ] 
+then 
+    echo "a is equal to b"
+fi 
+   
+if [ $a != $b ] 
+then 
+    echo "a is not equal to b"
+fi 
+
